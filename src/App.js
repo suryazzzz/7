f9c3770b7c4769935c6d90c3bb8323690c58843a
suryazzzz <118150056+suryazzzz@users.noreply.github.com>
2022-11-14 11:05:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import React, {useState} from 'react';
+import './Style.css';
+import ReactDOM from 'react-dom';
+import './App.css';
+
+function App() {
+	const [name , setName] = useState('');
+	const [age , setAge] = useState('');
+	const [email , setEmail] = useState('');
+	const [password , setPassword] = useState('');
+	const [confPassword , setConfPassword] = useState('');
+
+	const handleChange =(e)=>{
+	setName(e.target.value);
+	}
+	const handleAgeChange =(e)=>{
+	setAge(e.target.value);
+	}
+	
+	const handleEmailChange =(e)=>{
+	setEmail(e.target.value);
+	}
+	
+	const handlePasswordChange =(e)=>{
+	setPassword(e.target.value);
+	}
+	
+	const handleConfPasswordChange =(e)=>{
+	setConfPassword(e.target.value);
+	}
+	
+	const handleSubmit=(e)=>{
+	if(password!=confPassword)
+	{
+		
+		alert("password Not Match");
+	}
+	else{
+	
+		alert('A form was submitted with Name :"' + name +
+		'" ,Age :"'+age +'" and Email :"' + email + '"');
+	}
+	e.preventDefault();
+
+	}
+return (
+	<div className="App">
+	<header className="App-header">
+	<form onSubmit={(e) => {handleSubmit(e)}}>
+	{}
+	<h2> <i>REGISTERATION FORM</i> </h2>
+	
+		<label >
+		Name:
+		</label><br/>
+		<input type="text" value={name} required onChange={(e)=> {handleChange(e)}} /><br/>
+		{ }
+		<label >
+		Age:
+		</label><br/>
+		<input type="text" value={age} required onChange={(e)=> {handleAgeChange(e)}} /><br/>
+			{}
+		<label>
+		Email:
+		</label><br/>
+		<input type="email" value={email} required onChange={(e)=> {handleEmailChange(e)}} /><br/>
+		{}
+		<label>
+		Password:
+		</label><br/>
+		<input type="password" value={password} required onChange={(e)=> {handlePasswordChange(e)}} /><br/>
+			{}
+		<label>
+		Confirm Password:
+		</label><br/>
+		<input type="password" value={confPassword} required onChange={(e)=> {handleConfPasswordChange(e)}} /><br/>
+				{}
+		<input type="submit" value="Submit"/>
+	</form>
+	</header>
+	</div>
+);
+}
+
+export default App;
